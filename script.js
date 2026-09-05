c12f8e1bf39812c044bf8bab4eb0244e42fefca7
Rohit Paramanik <rohitparamanixyz@gmail.com>
2026-09-05 20:16:18 +0530

    Added save notes functionality

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
-const inputText = document.getElementById('input-text')
+import { notesData } from "./data.js"
 
-inputText.addEventListener('focus', () => {
-    inputText.setAttribute('rows', '8')
-})
+const saveBtn = document.getElementById('save-btn')
 
-inputText.addEventListener('blur', () => {
-    inputText.setAttribute('rows', '1')
-})
+saveBtn.addEventListener('click', addNotes)
+
+function addNotes() {
+    let noteTitleAreaValue = document.getElementById('note-title-area').value
+    let noteTextAreaValue = document.getElementById('note-text-area').value
+
+    if(noteTextAreaValue === '' && noteTitleAreaValue === '') {
+        alert('Nothing to save...')
+    } else {
+       let noteObj = {
+            title: `${noteTitleAreaValue}`,
+            text: `${noteTextAreaValue}`
+        }
+
+        notesData.push(noteObj)
+    }
+
+    document.getElementById('note-title-area').value = ''
+    document.getElementById('note-text-area').value = ''
+
+    renderNotes()
+
+}
+
+function renderNotes() {
+    let savedNotes = document.getElementById('saved-notes')
+
+    let savedNotesHtml = ``
+
+    notesData.forEach(element => {
+        savedNotesHtml += `
+        <div class="note">
+            <div class="note-content">
+               <h2>${element.title}</h2>
+               <p>${element.text}</p>
+            </div>
+            <button id="delete-note-btn" class="delete-note-btn>Delete</button>
+        </div>
+        `
+    });
+
+    savedNotes.innerHTML = savedNotesHtml
+}
